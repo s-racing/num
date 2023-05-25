d5802e45bb887c2359e13cadbfca69aceba5ebba
satish <satish8840123@gmail.com>
2023-05-25 16:48:04 +0530

    d

--- script.sh
+++ script.sh
@@ -1,11 +1,12 @@
-
 #!/bin/bash
-echo "Starting web page deployment..."
 
-cd /var/www/html/num
-sudo rm -f index.html
-cd /var/lib/jenkins/workspace/new
-sudo cp -R * /var/www/html/num/
+# Check to see if the current branch is the same as the branch that was merged
+if [[ $GIT_MERGE_HEAD != $three ]]; then
 
-echo "Web page deployment completed successfully."
+  # If the current branch is not the same as the branch that was merged, then deploy the changes
+  cd /var/www/html/num
+  sudo rm -f index.html
+  cd /var/lib/jenkins/workspace/new
+  sudo cp -R * /var/www/html/num/
 
+fi
