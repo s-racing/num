aadc03eabd4a507f2f15cbc66c711a5e1b20e3e8
satish <satish8840123@gmail.com>
2023-05-26 09:16:25 +0530

    f

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@
 
 # Name of the target branch (e.g., main)
 target_branch="main"
+branch_to_merge="three"
 
 # Deploy the changes if the current branch is the target branch
 if [[ "$(git symbolic-ref --short HEAD)" = "$target_branch" ]]; then
@@ -12,4 +13,14 @@ if [[ "$(git symbolic-ref --short HEAD)" = "$target_branch" ]]; then
 fi
 
 # Merge the branch into the target branch
-git checkout "$target_branch" && git merge --no-ff "three"
+if [[ "$(git symbolic-ref --short HEAD)" = "$branch_to_merge" ]]; then
+  git checkout "$target_branch" && git merge --no-ff "$branch_to_merge"
+fi
+
+# Deploy the changes to the web page after merging into the target branch
+if [[ "$(git symbolic-ref --short HEAD)" = "$target_branch" ]]; then
+  cd /var/www/html/num
+  sudo rm -f index.html
+  cd /var/lib/jenkins/workspace/new
+  sudo cp -R * /var/www/html/num/
+fi
