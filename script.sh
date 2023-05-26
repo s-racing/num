bcf4ac53004bb0349deaa6828d623aa57839ae2e
satish <satish8840123@gmail.com>
2023-05-26 09:08:24 +0530

    F

--- script.sh
+++ script.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-# Check to see if the current branch is the same as the branch that was merged
-if [[ $GIT_MERGE_HEAD != $three ]]; then
+# Name of the target branch (e.g., main)
+target_branch="main"
 
-  # If the current branch is not the same as the branch that was merged, then deploy the changes
-  cd /var/www/html/num
-  sudo rm -f index.html
-  cd /var/lib/jenkins/workspace/new
-  sudo cp -R * /var/www/html/num/
+# Check if the current branch is the same as the branch to merge
+if [[ "$(git symbolic-ref --short HEAD)" = "three" ]]; then
+  # If the current branch is the branch to merge, merge it into the target branch
+  git checkout "$target_branch" && git merge --no-ff "three"
 
+  # If the merge was successful, deploy the changes
+  if [ $? -eq 0 ]; then
+    cd /var/www/html/num
+    sudo rm -f index.html
+    cd /var/lib/jenkins/workspace/new
+    sudo cp -R * /var/www/html/num/
+  else
+    echo "Merge failed. Please resolve conflicts manually."
+  fi
 fi
