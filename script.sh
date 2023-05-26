a60966ea75fc2ae5f6f28be16988cb31cd1e3dfd
satish <satish8840123@gmail.com>
2023-05-26 09:45:41 +0530

    s

--- script.sh
+++ script.sh
@@ -2,7 +2,6 @@
 
 # Name of the target branch (e.g., main)
 target_branch="main"
-branch_to_merge="one"
 
 # Function to deploy changes to the web page
 deploy_changes() {
@@ -12,23 +11,10 @@ deploy_changes() {
   sudo cp -R * /var/www/html/num/
 }
 
-# Get the current branch name
-current_branch=$(git rev-parse --abbrev-ref HEAD)
-
-# Merge the branch into the target branch
-if [[ "$current_branch" = "$branch_to_merge" ]]; then
-  git checkout "$target_branch" && git merge --no-ff "$branch_to_merge"
-
-  # Check if the merge was successful
-  if [ $? -eq 0 ]; then
-    # Deploy changes if merged successfully
-    deploy_changes
-  else
-    echo "Merge failed. Changes will not be deployed."
-  fi
-fi
-
-# Deploy changes if the current branch is the target branch
-if [[ "$current_branch" = "$target_branch" ]]; then
+# Check if the current branch is the target branch
+if [[ "$(git rev-parse --abbrev-ref HEAD)" = "$target_branch" ]]; then
+  echo "Deploying changes..."
   deploy_changes
+else
+  echo "Current branch is not '$target_branch'. Changes will not be deployed."
 fi
